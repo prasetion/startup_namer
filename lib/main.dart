dea6569d280e523ef6825953b80f788231e0810e
prasetion <prasetion@gmail.com>
2018-03-25 12:40:49 +0700

    changing theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget{
 
     return new MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: new RandomWords()
     );
 
@@ -31,7 +34,7 @@ class RandomWordsState extends State<RandomWords>{
   @override
   Widget build(BuildContext context){
      return new Scaffold(
-      appBar: new AppBar(title: new Text('Startup Name Generator')),
+      appBar: new AppBar(title: new Text('Startup Name Generator'), actions: <Widget>[new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved,)],),
       body: _buildSuggestions(),
     );
   }
@@ -73,7 +76,31 @@ class RandomWordsState extends State<RandomWords>{
       },
     );
   
+  }
 
+  void _pushSaved(){
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context){
+          final tiles = _saved.map(
+            (pair){
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase, style: _biggerFont,
+                ),
+              );
+            }
+          );
+
+          final divided = ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions')), 
+              body: new ListView(children: divided,),);
+        }, 
+      ),
+    );
   }
 
 }
